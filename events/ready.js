f6ffef7c15486c8b52b79b7b0f2512cd770720cc
Fiezt <dat09t@gmail.com>
2021-09-26 15:21:37 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,16 +1,5 @@
-const Discord = require('discord.js')
 module.exports = async (bot) => {
   
   console.log(`Logged in as ${bot.user.username}. Ready on ${bot.guilds.cache.size} servers, for a total of ${bot.users.cache.size} users`);
-
-  // const actvs = config.discord.activity.name(bot);
-  // let i = 0;
-
-  // setInterval(() => {
-  //   if (i === actvs.length) i = 0;
-  //   const stt = actvs[i];
-  //   bot.user.setActivity(stt, { type: config.discord.activity.type }).catch(console.error)
-  //   i++;
-  // }, 300000);
 
 };

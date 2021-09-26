8cc5b24b7cb627e460518459d2e577c22ef9a170
Fiezt <dat09t@gmail.com>
2021-09-26 15:15:33 +0700

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -1,29 +1,5 @@
 
 module.exports = {
-  emojis: {
-    success: [
-      "<:Cool_Finger_Guns:852818433002438686>",
-      "<a:clappypepe:852756175752658974>",
-      "<a:vibecat:852756181822865408>",
-      "<a:_pogrow:852756177678630912>",
-      "<a:party_pug_dance:852756181491646534>"
-    ],
-    
-    fail: [
-      "<a:BonkHammer:852955161084166164>",
-      "<:_PepeBlanket:852818107759067137> ",
-      "<a:bongopepe:852815592691990569> ",
-      "<a:eto:852957041408213002>"
-    ]
-  },
-  quotes: [
-    "Battle with your friend will receive a good amount of owlet than battle with monsters.",
-    "Earn more owlet to upgrade your equipment or buy things.",
-    "You can try you luck with blackjack, cointoss and so on.",
-    "You can change your Battle theme.",
-    "Once your level ups, you will have 5 extra points to upgrade your skill.",
-    "Equipments are diversity, they can be used by many other classes."
-  ],
   discord: {
     botid: process.env.BOT_ID,
     prefix: process.env.PREFIX,
